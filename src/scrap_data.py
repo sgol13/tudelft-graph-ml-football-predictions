5d51fae0b43ea8f9d91696473eb65b52e6ecf44c
Szymon Golebiowski <szymon13gol@gmail.com>
2025-09-26 17:50:56 +0200

    scraping data

---
+++ src/scrap_data.py
@@ -0,0 +1,29 @@
+import soccerdata as sd
+from tqdm import tqdm
+import pickle
+
+
+def scrap_data(season: int):
+    ws = sd.WhoScored(leagues="ENG-Premier League", seasons=season)
+
+    epl_schedule = ws.read_schedule()
+
+    matches = epl_schedule.to_dict(orient='records')
+
+    for match in tqdm(matches):
+        game_id = match['game_id']
+        events = ws.read_events(match_id=game_id)
+        match['events'] = events
+
+    filename = f"data/epl_{season}.pkl"
+    with open(filename, "wb") as f:
+        pickle.dump(matches, f)
+
+
+def main():
+    for year in range(2015, 2024):
+        scrap_data(year)
+
+
+if __name__ == "__main__":
+    main()
